9563c535f681b6f2a61a9c7e43e2433d001cbefc
Filip Kulig <filip.kulig@3t-transform.com>
2023-11-04 22:38:00 +0000

    Changes

--- route53-record-updater/main.py
+++ route53-record-updater/main.py
@@ -1,6 +1,13 @@
 import sqlite3
 from sqlite3 import Error
 import requests
+import os.path
+import os
+import datetime
+
+
+ENVS = ['USERNAME']
+
 
 def create_connection(db_file):
 
@@ -13,46 +20,72 @@ def create_connection(db_file):
     return conn
 
 
-def create_project(conn, project):
-
-    sql = ''' INSERT INTO projects(name,begin_date,end_date)
-              VALUES(?,?,?) '''
+def create_ip(conn, ip):
+    data = [ip]
+    sql = "INSERT INTO ip(ip) VALUES(?)"
     cur = conn.cursor()
-    cur.execute(sql, project)
+    cur.execute(sql, data)
     conn.commit()
     return cur.lastrowid
 
 
-def create_ip(conn):
-    sql_create_ip_table = \
-    """ CREATE TABLE IF NOT EXISTS ip (
-        id integer PRIMARY KEY,
-        ip text NOT NULL
-    ); """
-    try:
-        c = conn.cursor()
-        c.execute(sql_create_ip_table)
-    except Error as e:
-        print(e)
-    pass
-
-
 def get_ip():
-    ip = get('https://api.ipify.org').content.decode('utf8')
-
+    ip = requests.get('https://api.ipify.org').content.decode('utf8')
     return ip
 
+
+def db_exists():
+    return os.path.isfile("ip.db")
+
+
+def create_db():
+    # TODO: do the db creation stuff
+    sql_create_ip_table = """ CREATE TABLE IF NOT EXISTS ip (
+                                         id integer PRIMARY KEY,
+                                         ip TEXT NOT NULL,
+                                         date TEXT NOT NULL,
+                                         name text NOT NULL
+                                     ); """
+
+    db = r"ip.db"
+    conn = create_connection(db)
+    c = conn.cursor()
+    try:
+        c.execute(sql_create_ip_table)
+    except Error as e:
+        return False
+
+    return True
+
+
+def get_env(name):
+    name = f"RCT_{name}"
+    if name in os.environ:
+        return os.environ[name]
+    else:
+        return False
+
+
 def main():
+
+    username = get_env('USERNAME')
+    print(username)
+    if not username:
+        raise Exception("RCT_USERNAME env variable  not found")
+
     database = r"ip.db"
-    ip = get_ip()
-    print(ip)
+    if not db_exists():
+        status = create_db()
+        if not status:
+            print("db creation fail, oh no")
+
     # create a database connection
     conn = create_connection(database)
-    create_ip(conn)
     with conn:
         # create a new project
-        project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30');
-        project_id = create_project(conn, project)
+        ip = get_ip()
+        ip_id = create_ip(conn, ip)
+        print(ip_id)
 
 
 if __name__ == '__main__':
